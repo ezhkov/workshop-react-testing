bc4f54f8cd72e174ce51717f7d0e3875f7a15a59
Denis Ezhkov <Dan.SKIff@gmail.com>
2019-02-23 17:56:38 +0400

    Added new tests

--- __tests__/Tabs.test.jsx
+++ __tests__/Tabs.test.jsx
@@ -1,14 +1,73 @@
 import React from 'react';
-import { mount } from 'enzyme';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from '../src/components/App';
 
-describe('Check if app renders', () => {
-  it('render', () => {
+Enzyme.configure({ adapter: new Adapter() });
+
+const getTabsList = wrapper => wrapper.find('li[data-test="tab"]');
+const getTabsContents = wrapper => wrapper.find('div[data-test="tabContent"]');
+
+describe('Tabs Render', () => {
+  it('select last tab', () => {
     const wrapper = mount(<App />);
-    const tabs = wrapper.find('[data-test="tab"]');
+    const tabs = getTabsList(wrapper);
+    const lastTab = tabs.last();
+    expect(wrapper.render()).toMatchSnapshot();
+    lastTab.simulate('click');
+    expect(wrapper.render()).toMatchSnapshot();
+  });
+});
+
+describe('Tabs Actions', () => {
+  it('tab has activated', () => {
+    const wrapper = mount(<App />);
+    const tabs = getTabsList(wrapper);
     const lastTab = tabs.last();
     lastTab.simulate('click');
-    const lastTabContent = wrapper.find('[data-test="tabContent"]').last();
-    expect(lastTabContent).toMatchSnapshot();
+    const newTabs = getTabsList(wrapper);
+    const newLastTab = newTabs.last();
+    expect(newLastTab).toMatchSelector('[aria-selected="true"]');
   });
+
+  it('content is visible', () => {
+    const wrapper = mount(<App />);
+    const tabs = getTabsList(wrapper);
+    const tab = tabs.at(3);
+    tab.simulate('click');
+    const tabContents = getTabsContents(wrapper);
+    const needleContent = tabContents.at(3);
+    expect(needleContent.children()).toExist();
+  });
+
+  it('tab has added', () => {
+    const wrapper = mount(<App />);
+    const tabs = getTabsList(wrapper);
+    const lastIndexToAdd = tabs.length;
+    const addTab = wrapper.find('[data-test="addTab"]');
+    addTab.simulate('click');
+    const newTabs = getTabsList(wrapper);
+    expect(newTabs.at(lastIndexToAdd)).toExist();
+  });
+
+  it('tab has removed', () => {
+    const wrapper = mount(<App />);
+    const tabs = getTabsList(wrapper);
+    const tabsCount = tabs.length;
+    const removeTab = tabs.at(2).find('[data-test="removeTab"]');
+    removeTab.simulate('click');
+    const newTabs = getTabsList(wrapper);
+    expect(newTabs.at(tabsCount - 1)).not.toExist();
+  });
+
+  it('exact tab has removed', () => {
+    const wrapper = mount(<App />);
+    const tabs = getTabsList(wrapper);
+    const needleTab = tabs.at(2);
+    const needleTabRemove = needleTab.find('[data-test="removeTab"]');
+    needleTabRemove.simulate('click');
+    const newTabs = getTabsList(wrapper);
+    expect(newTabs.contains(needleTab)).toBeFalsy();
+  });
+
 });
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,31 +2,102 @@ import React from 'react';
 import {
   Tab, Tabs as TabsContainer, TabList, TabPanel,
 } from 'react-tabs';
+import nanoid from 'nanoid';
 
 import 'react-tabs/style/react-tabs.css';
 
-export default function Tabs() {
-  return (
-    <TabsContainer>
-      <TabList>
-        <Tab><span data-test="tab">Tab header 1</span></Tab>
-        <Tab><span data-test="tab">Tab header 2</span></Tab>
-        <Tab><span data-test="tab">Tab header 3</span></Tab>
-        <Tab><span data-test="tab">Tab header 4</span></Tab>
-      </TabList>
+export default class Tabs extends React.Component {
+  state = {
+    tabs: [
+      {
+        title: 'Tab header 1',
+        content: 'Tab panel 1 content',
+        uid: nanoid(),
+      },
+      {
+        title: 'Tab header 2',
+        content: 'Tab panel 2 content',
+        uid: nanoid(),
+      },
+      {
+        title: 'Tab header 3',
+        content: 'Tab panel 3 content',
+        uid: nanoid(),
+      },
+      {
+        title: 'Tab header 4',
+        content: 'Tab panel 4 content',
+        uid: nanoid(),
+      },
+    ],
+  };
 
-      <TabPanel>
-        <div data-test="tabContent">Tab panel 1 content</div>
+  makeTab = () => {
+    const { tabs } = this.state;
+    return {
+      title: `Tab header ${tabs.length + 1}`,
+      content: `Tab panel ${tabs.length + 1} content`,
+      uid: nanoid(),
+    };
+  };
+
+  handleRemoveTab = index => () => {
+    const { activeTabIndex } = this.state;
+    if (index !== activeTabIndex) {
+      this.setState((prevState) => {
+        const newTabs = [...prevState.tabs];
+        newTabs.splice(index, 1);
+        return {
+          tabs: newTabs,
+        };
+      });
+    }
+  };
+
+  handleAddTab = () => {
+    const newTab = this.makeTab();
+    this.setState(prevState => ({
+      tabs: [
+        ...prevState.tabs,
+        newTab,
+      ],
+    }));
+  };
+
+  renderTabsList() {
+    const { tabs } = this.state;
+    return tabs.map((elem, index) => (
+      <Tab key={elem.uid} data-test="tab">
+        <button type="button" data-test="removeTab" onClick={this.handleRemoveTab(index)}>X</button>
+        <span>{ elem.title }</span>
+      </Tab>
+    ));
+  }
+
+  renderTabsPanels() {
+    const { tabs } = this.state;
+    return tabs.map(elem => (
+      <TabPanel key={elem.uid} data-test="tabContent">
+        <div>
+          { elem.content }
+        </div>
       </TabPanel>
-      <TabPanel>
-        <div data-test="tabContent">Tab panel 2 content</div>
-      </TabPanel>
-      <TabPanel>
-        <div data-test="tabContent">Tab panel 3 content</div>
-      </TabPanel>
-      <TabPanel>
-        <div data-test="tabContent">Tab panel 4 content</div>
-      </TabPanel>
-    </TabsContainer>
-  );
+    ));
+  }
+
+  render() {
+    return (
+      <>
+        <div>
+          <button data-test="addTab" type="button" onClick={this.handleAddTab}>Add tab</button>
+        </div>
+        <TabsContainer>
+          <TabList data-test="tabs-box">
+            { this.renderTabsList() }
+          </TabList>
+          { this.renderTabsPanels() }
+        </TabsContainer>
+      </>
+    );
+  }
 }
